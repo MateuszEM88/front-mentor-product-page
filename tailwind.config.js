dbf7e19e3494c801c304f2b3a079dd568322140b
MateuszEM88 <108663597+MateuszEM88@users.noreply.github.com>
2023-02-10 14:50:17 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./index.html"],
   theme: {
     colors: {
       darkCyan: "hsl(158, 36%, 37%)",
